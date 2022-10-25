e2747cc1c1aae022d62f45d26e527f3033ad5ce0
junp <junpc97@gmail.com>
2022-10-25 23:52:06 +0800

    temp

--- board.hpp
+++ board.hpp
@@ -2,11 +2,270 @@
 
 namespace nara {
 
+struct point_t {
+    int x;
+    int y;
+    point_t(int _x = 0, int _y = 0): x(_x), y(_y) {}
+};
+
+const point_t directions[4] = {
+    point_t{1, 0}, point_t{1, 1}, point_t{0, 1}, point_t{-1, 1}
+};
+
+bool operator == (point_t p1, point_t p2) {
+    return p1.x == p2.x && p1.y == p2.y;
+}
+
+using points_t = std::vector<point_t>;
+
+points_t mkline(point_t center, point_t dir) {
+    points_t points;
+    for(int fac = -4; fac <= 4; fac++) {
+        points.push_back({
+            center.x + fac * dir.x, center.y + fac * dir.y
+        });
+    }
+    return points;
+}
+
+enum gomoku_chess { EMPTY, OUTBX, BLACK, WHITE };
+
+gomoku_chess oppof(gomoku_chess chess) {
+    assert(chess == BLACK || chess == WHITE);
+    return chess == BLACK ? WHITE : BLACK;
+}
+
+class gomoku_board {
+public:
+    static const int WIDTH = 15;
+
+    gomoku_chess chesses[WIDTH][WIDTH];
+
+    int winner;
+
+    gomoku_board() {
+        winner = 0;
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j < WIDTH; j++) {
+                chesses[i][j] = EMPTY;
+            }
+        }
+    }
+
+    static constexpr inline bool outbox(int x, int y) {
+        return x < 0 || x >= WIDTH || y < 0 || y >= WIDTH;
+    }
+
+    static inline bool outbox(point_t pos) {
+        return outbox(pos.x, pos.y);
+    }
+
+    gomoku_chess getchess(int x, int y) const {
+        return outbox(x, y) ? OUTBX : chesses[x][y];
+    }
+
+    gomoku_chess getchess(point_t pos) const {
+        return getchess(pos.x, pos.y);
+    }
+
+    void setchess(int x, int y, gomoku_chess chess) {
+        assert(x >= 0 && x < WIDTH);
+        assert(y >= 0 && y < WIDTH);
+        chesses[x][y] = chess;
+    }
+
+    void setchess(point_t pos, gomoku_chess chess) {
+        setchess(pos.x, pos.y, chess);
+    }
+
+    void setchess_and_test(int x, int y, gomoku_chess chess) {
+        if(winner != 0) return;
+        setchess(x, y, chess);
+        winner = testpoint(x, y);
+    }
+
+    bool iswin(points_t const & points, int start, int stop) {
+        assert(stop - start == 5);
+
+        auto begin = points.begin() + start;
+        auto end = points.begin() + stop;
+
+        gomoku_chess one = getchess(*begin);
+
+        if(one == EMPTY)
+            return false;
+
+        for(auto point = begin + 1; point != end; point++) {
+            if(getchess(*point) != one)
+                return false;
+        }
+
+        return true;
+    }
+
+    points_t testwin(points_t const & points) {
+        assert(points.size() == 9);
+        for(int i = 0; i < 5; i++) {
+            if(iswin(points, i, i+5))
+                return points_t(points.begin()+i, points.begin()+i+5);
+        }
+        return points_t{};
+    }
+
+    // 1 for BLACK WIN, -1 for WHITE WIN, 0 for NO WINNER
+    int testpoint(int x, int y) {
+        if(getchess(x, y) == EMPTY)
+            return 0;
+
+        point_t center{x, y};
+        for(auto & dir : directions) {
+            auto points = mkline(center, dir);
+            auto winset = testwin(points);
+            if(winset.empty())
+                continue;
+            return getchess(winset[0]) == BLACK ? 1 : -1;
+        }
+
+        return 0;
+    }
+
+    void debugpoint(points_t points) {
+        for(auto const & point : points) {
+            std::cout << '[' << point.x << ", " << point.y << ']';
+        }
+        std::cout << '\n';
+    }
+
+    int evalwinner(void) {
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j < WIDTH; j++) {
+                int res = testpoint(i, j);
+                if(res != 0)
+                    return res;
+            }
+        }
+        return 0;
+    }
+};
+
 enum gomoku_chess { EMPTY, OUTBX, BLACK, WHITE };
 
 gomoku_chess oppof(gomoku_chess chess) {
     // assert(chess == BLACK || chess == WHITE);
     return chess == BLACK ? WHITE : BLACK;
 }
+
+class gomoku_board {
+public:
+    static const int WIDTH = 15;
+
+    gomoku_chess chesses[WIDTH][WIDTH];
+
+    int winner;
+
+    gomoku_board() {
+        winner = 0;
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j < WIDTH; j++) {
+                chesses[i][j] = EMPTY;
+            }
+        }
+    }
+
+    static constexpr inline bool outbox(int x, int y) {
+        return x < 0 || x >= WIDTH || y < 0 || y >= WIDTH;
+    }
+
+    static inline bool outbox(point_t pos) {
+        return outbox(pos.x, pos.y);
+    }
+
+    gomoku_chess getchess(int x, int y) const {
+        return outbox(x, y) ? OUTBX : chesses[x][y];
+    }
+
+    gomoku_chess getchess(point_t pos) const {
+        return getchess(pos.x, pos.y);
+    }
+
+    void setchess(int x, int y, gomoku_chess chess) {
+        assert(x >= 0 && x < WIDTH);
+        assert(y >= 0 && y < WIDTH);
+        chesses[x][y] = chess;
+    }
+
+    void setchess(point_t pos, gomoku_chess chess) {
+        setchess(pos.x, pos.y, chess);
+    }
+
+    void setchess_and_test(int x, int y, gomoku_chess chess) {
+        if(winner != 0) return;
+        setchess(x, y, chess);
+        winner = testpoint(x, y);
+    }
+
+    bool iswin(points_t const & points, int start, int stop) {
+        assert(stop - start == 5);
+
+        auto begin = points.begin() + start;
+        auto end = points.begin() + stop;
+
+        gomoku_chess one = getchess(*begin);
+
+        if(one == EMPTY)
+            return false;
+
+        for(auto point = begin + 1; point != end; point++) {
+            if(getchess(*point) != one)
+                return false;
+        }
+
+        return true;
+    }
+
+    points_t testwin(points_t const & points) {
+        assert(points.size() == 9);
+        for(int i = 0; i < 5; i++) {
+            if(iswin(points, i, i+5))
+                return points_t(points.begin()+i, points.begin()+i+5);
+        }
+        return points_t{};
+    }
+
+    // 1 for BLACK WIN, -1 for WHITE WIN, 0 for NO WINNER
+    int testpoint(int x, int y) {
+        if(getchess(x, y) == EMPTY)
+            return 0;
+
+        point_t center{x, y};
+        for(auto & dir : directions) {
+            auto points = mkline(center, dir);
+            auto winset = testwin(points);
+            if(winset.empty())
+                continue;
+            return getchess(winset[0]) == BLACK ? 1 : -1;
+        }
+
+        return 0;
+    }
+
+    void debugpoint(points_t points) {
+        for(auto const & point : points) {
+            std::cout << '[' << point.x << ", " << point.y << ']';
+        }
+        std::cout << '\n';
+    }
+
+    int evalwinner(void) {
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j < WIDTH; j++) {
+                int res = testpoint(i, j);
+                if(res != 0)
+                    return res;
+            }
+        }
+        return 0;
+    }
+};
 
 } // namespace nara
--- main.cpp
+++ main.cpp
@@ -16,152 +16,6 @@
 
 std::ofstream logger;
 
-struct point_t {
-    int x;
-    int y;
-    point_t(int _x = 0, int _y = 0): x(_x), y(_y) {}
-};
-
-const point_t directions[4] = {
-    point_t{1, 0}, point_t{1, 1}, point_t{0, 1}, point_t{-1, 1}
-};
-
-bool operator == (point_t p1, point_t p2) {
-    return p1.x == p2.x && p1.y == p2.y;
-}
-
-using points_t = std::vector<point_t>;
-
-points_t mkline(point_t center, point_t dir) {
-    points_t points;
-    for(int fac = -4; fac <= 4; fac++) {
-        points.push_back({
-            center.x + fac * dir.x, center.y + fac * dir.y
-        });
-    }
-    return points;
-}
-
-enum gomoku_chess { EMPTY, OUTBX, BLACK, WHITE };
-
-gomoku_chess oppof(gomoku_chess chess) {
-    assert(chess == BLACK || chess == WHITE);
-    return chess == BLACK ? WHITE : BLACK;
-}
-
-class gomoku_board {
-public:
-    static const int WIDTH = 15;
-
-    gomoku_chess chesses[WIDTH][WIDTH];
-
-    int winner;
-
-    gomoku_board() {
-        winner = 0;
-        for(int i = 0; i < WIDTH; i++) {
-            for(int j = 0; j < WIDTH; j++) {
-                chesses[i][j] = EMPTY;
-            }
-        }
-    }
-
-    static constexpr inline bool outbox(int x, int y) {
-        return x < 0 || x >= WIDTH || y < 0 || y >= WIDTH;
-    }
-
-    static inline bool outbox(point_t pos) {
-        return outbox(pos.x, pos.y);
-    }
-
-    gomoku_chess getchess(int x, int y) const {
-        return outbox(x, y) ? OUTBX : chesses[x][y];
-    }
-
-    gomoku_chess getchess(point_t pos) const {
-        return getchess(pos.x, pos.y);
-    }
-
-    void setchess(int x, int y, gomoku_chess chess) {
-        assert(x >= 0 && x < WIDTH);
-        assert(y >= 0 && y < WIDTH);
-        chesses[x][y] = chess;
-    }
-
-    void setchess(point_t pos, gomoku_chess chess) {
-        setchess(pos.x, pos.y, chess);
-    }
-
-    void setchess_and_test(int x, int y, gomoku_chess chess) {
-        if(winner != 0) return;
-        setchess(x, y, chess);
-        winner = testpoint(x, y);
-    }
-
-    bool iswin(points_t const & points, int start, int stop) {
-        assert(stop - start == 5);
-
-        auto begin = points.begin() + start;
-        auto end = points.begin() + stop;
-
-        gomoku_chess one = getchess(*begin);
-
-        if(one == EMPTY)
-            return false;
-
-        for(auto point = begin + 1; point != end; point++) {
-            if(getchess(*point) != one)
-                return false;
-        }
-
-        return true;
-    }
-
-    points_t testwin(points_t const & points) {
-        assert(points.size() == 9);
-        for(int i = 0; i < 5; i++) {
-            if(iswin(points, i, i+5))
-                return points_t(points.begin()+i, points.begin()+i+5);
-        }
-        return points_t{};
-    }
-
-    // 1 for BLACK WIN, -1 for WHITE WIN, 0 for NO WINNER
-    int testpoint(int x, int y) {
-        if(getchess(x, y) == EMPTY)
-            return 0;
-
-        point_t center{x, y};
-        for(auto & dir : directions) {
-            auto points = mkline(center, dir);
-            auto winset = testwin(points);
-            if(winset.empty())
-                continue;
-            return getchess(winset[0]) == BLACK ? 1 : -1;
-        }
-
-        return 0;
-    }
-
-    void debugpoint(points_t points) {
-        for(auto const & point : points) {
-            std::cout << '[' << point.x << ", " << point.y << ']';
-        }
-        std::cout << '\n';
-    }
-
-    int evalwinner(void) {
-        for(int i = 0; i < WIDTH; i++) {
-            for(int j = 0; j < WIDTH; j++) {
-                int res = testpoint(i, j);
-                if(res != 0)
-                    return res;
-            }
-        }
-        return 0;
-    }
-};
-
 struct cursor_t {
     int x;
     int y;
@@ -202,7 +56,7 @@ static int last_eval_times;
 auto evaluate_line(gomoku_board const & board, line_t line) {
     last_eval_times++;
 
-    auto start = std::chrono::system_clock::now();
+    // auto start = std::chrono::system_clock::now();
 
     std::vector<nara::gomoku_chess> chesses;
 
@@ -219,11 +73,13 @@ auto evaluate_line(gomoku_board const & board, line_t line) {
         p = {p.x + dir.x, p.y + dir.y};
     }
 
+    /*
     auto end = std::chrono::system_clock::now();
     logger << "evaluate_line use"
            << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
            << "\n";
     logger.flush();
+    */
 
     return nara::evaluate(chesses);
 }
@@ -433,6 +289,63 @@ private:
         std::ranges::sort(candidates, compare_priority);
     }
 
+    struct choose_t {
+        point_t pos;
+        int score_blk;
+        int score_wht;
+        int score;
+    };
+
+    std::vector<point_t>
+    line_for_pos(point_t pos, point_t dir) {
+        std::vector<point_t> line;
+        for(int fac = -4; fac <= 4; fac++) {
+            line.emplace_back(point_t{
+                pos.x + fac * dir.x, pos.y + fac * dir.y
+            });
+        }
+        return line;
+    }
+
+    std::vector<nara::gomoku_chess>
+    gen_chesses(gomoku_board const & board, std::vector<point_t> line) {
+        std::vector<nara::gomoku_chess> chesses;
+        for(auto & p : line) {
+            switch(board.getchess(p)) {
+            case BLACK: chesses.push_back(nara::BLACK); break;
+            case WHITE: chesses.push_back(nara::WHITE); break;
+            case EMPTY: chesses.push_back(nara::EMPTY); break;
+            // case OUTBX: chesses.push_back(nara::OUTBX); break;
+            }
+        }
+        return chesses;
+    }
+
+    choose_t gen_choose(gomoku_board const & board, point_t pos) {
+        assert(board.getchess(pos) == EMPTY);
+        choose_t choose{pos, 0, 0, 0};
+        for(auto dir : directions) {
+            auto line = line_for_pos(pos, dir);
+            auto chesses = gen_chesses(board, line);
+            auto [rblk, rwht] = evaluate(chesses);
+            choose.score_blk += rblk.score;
+            choose.score_wht += rwht.score;
+        }
+        choose.score = (mine == BLACK)
+            ? choose.score_blk - choose.score_wht
+            : choose.score_wht - choose.score_blk;
+
+        return choose;
+    }
+
+    static bool with_score(choose_t choose) {
+        return choose.score_blk != 0 || choose.score_wht != 0;
+    }
+
+    auto choose_generator(gomoku_board const & board) {
+        return [&](point_t pos){ return gen_choose(board, pos); };
+    }
+
     std::tuple<point_t, int> alphabeta(
         score_board const & sboard, gomoku_chess next,
         int alpha, int beta, bool ismax, int depth
@@ -446,23 +359,36 @@ private:
                 return std::make_tuple(sboard.last_pos, sboard.score_wht());
         }
 
-        std::vector<point_t> poses;
+        std::vector<choose_t> chooses;
         std::ranges::copy(
-            candidates | std::views::filter(is_empty_at(sboard)),
-            std::back_inserter(poses)
+            candidates |
+            std::views::filter(is_empty_at(sboard)) |
+            std::views::transform(choose_generator(sboard._board)) |
+            std::views::filter(with_score),
+            std::back_inserter(chooses)
         );
+
+        if(chooses.empty()) {
+            std::ranges::copy(
+                candidates |
+                std::views::filter(is_empty_at(sboard)) |
+                std::views::transform(choose_generator(sboard._board)) |
+                std::views::take(9),
+                std::back_inserter(chooses)
+            );
+        }
 
         point_t bestpos;
 
         if(ismax) {
             int score = -10000000;
-            for(auto & pos : poses) {
+            for(auto & choose : chooses) {
 
                 auto[_ , _score] =
-                    alphabeta(score_board(sboard, next, pos), oppof(next), alpha, beta, false, depth - 1);
+                    alphabeta(score_board(sboard, next, choose.pos), oppof(next), alpha, beta, false, depth - 1);
 
                 if(_score > score)
-                    bestpos = pos;
+                    bestpos = choose.pos;
 
                 score = std::max(score, _score);
                 alpha = std::max(alpha, score);
@@ -475,13 +401,13 @@ private:
 
         // ismin
         int score = 10000000;
-        for(auto & pos : poses) {
+        for(auto & choose : chooses) {
 
             auto[_ , _score] =
-                alphabeta(score_board(sboard, next, pos), oppof(next), alpha, beta, true, depth - 1);
+                alphabeta(score_board(sboard, next, choose.pos), oppof(next), alpha, beta, true, depth - 1);
 
             if(_score < score)
-                bestpos = pos;
+                bestpos = choose.pos;
 
             score = std::min(score, _score);
             beta = std::min(beta, score);
@@ -607,6 +533,9 @@ int main() {
             auto elapsed = benchmark([&](){ ai_next = ai.getnext(board); });
             logger << "get next elapsed " << elapsed << ", eval times " << last_eval_times << '\n';
             logger.flush();
+
+            if(board.getchess(ai_next) != EMPTY)
+                assert(false);
 
             board.setchess_and_test(ai_next.x, ai_next.y, ai_chess);
             cursor.set(ai_next.x, ai_next.y);
